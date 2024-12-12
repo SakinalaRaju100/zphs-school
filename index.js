9bee493dabe3583ea250abc6794a81bbb9540753
raju.sakinala <raju.sakinala@apollohl.com>
2024-12-12 21:41:15 +0530

    kdh

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname)));
 
 // new
 mongoose.connect(
