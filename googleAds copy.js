4e9b46b014c40d9b62a30d991e9a444032f019ad
raju.sakinala <raju.sakinala@apollohl.com>
2024-12-13 20:35:32 +0530

    updated

---
+++ googleAds copy.js
@@ -0,0 +1,93 @@
+let a = [
+  {
+    SNo: 1,
+    page: 1,
+    title: "ZPHS School",
+    image:
+      "https://drive.google.com/thumbnail?id=1-6hCnYvJG0XbWYKVrjoc7ca-Vd59SCUx&sz=w1000",
+  },
+  {
+    SNo: 2,
+    page: 2,
+    title: "Hiranmai Constructions And Precast Materials",
+    image:
+      "https://drive.google.com/thumbnail?id=1uwswwB8LFIsA3q9mTQUrMO5RY0csA4Ht&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 4,
+    title: "Sai Bhavani motors And Goldnoon",
+    image:
+      "https://drive.google.com/thumbnail?id=1BNn7Ri0X-9kXJDEBreN97Bct_SHyUVGD&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 8,
+    title: "Neelima Book Bank",
+    image:
+      "https://drive.google.com/thumbnail?id=1OTMpkCS52MoQhqUU6gAVgZy7G7IJ05HC&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 12,
+    title: "Rudra Power Point",
+    image:
+      "https://drive.google.com/thumbnail?id=1gd1eyv53jOLoSVnp6eBDWWxhzHGJX9-u&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 19,
+    title: "Geeta Auto Commercials",
+    image:
+      "https://drive.google.com/thumbnail?id=1eYrnqFB_zvFZFbqdGWVBuqxci3KZnTl_&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 22,
+    title: "Anjana Chit Funds",
+    image:
+      "https://drive.google.com/thumbnail?id=1ks4BD6a4FtgwqLL9Me-YzQlpdV66q_IX&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 24,
+    title: "ZPHS Principals",
+    image:
+      "https://drive.google.com/thumbnail?id=1U5KimFJeKyc8gHMFTIFSKP4FeQoUTVAn&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 27,
+    title: "Allphores IIT & NEET Academy",
+    image:
+      "https://drive.google.com/thumbnail?id=1zYYvYMmbvGF0sLQriIoE5RRnrvffz-8P&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 29,
+    title: "Natural & Pure Oils",
+    image:
+      "https://drive.google.com/thumbnail?id=1mGvqa9ffjJ0FoxRY91g3StcjiIp4zl3E&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 35,
+    title: "Annapurna Seeds & Pesticides",
+    image:
+      "https://drive.google.com/thumbnail?id=1jhBS7tfcEw5uvKi_rgzIYVw-vYTaUgiT&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 39,
+    title: "CIVILDEN Green infra pvt.ltd",
+    image:
+      "https://drive.google.com/thumbnail?id=1jQb4C1pyG8QXCwtl4w4aTrTXaitBZ4Ot&sz=w1000",
+  },
+  {
+    SNo: 3,
+    page: 42,
+    title: "Cable tech Industries",
+    image:
+      "https://drive.google.com/thumbnail?id=1eCr3FbNPwhE8WCGjXTybCZnhmFDsiCeq&sz=w1000",
+  },
+];
