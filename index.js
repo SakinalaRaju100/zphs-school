d23137459aaa0918abf7f9ae866a0cea15f46319
raju.sakinala <raju.sakinala@apollohl.com>
2024-12-15 22:59:35 +0530

    wu

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 // const cors = require('cors');
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://react-vite-app-seven.vercel.app/"], // Allow only this origin
+  // origin: ["http://localhost:5173", "https://react-vite-app-seven.vercel.app/"], // Allow only this origin
+  origin: ["*"], // Allow only this origin
   methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
   credentials: true, // Allow cookies and authorization headers
 };
